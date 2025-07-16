3d38da1cccd7924b84c79428aaaba80e22b369c8
vikas kumar <vikaskumarkrishna01@gmail.com>
2025-07-16 16:08:51 +0530

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,7 @@ const cors = require('cors');
 // Update: Allow credentials and set specific origin for CORS
 const corsOptions = {
   origin: "http://localhost:5173",
+  origin: 'https://storeflick.onrender.com',
   credentials: true,
 };
 app.use(cors(corsOptions));
